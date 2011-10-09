30b8d95184ba8f29567fa3f0dcc24e6caf64ebd8
Maciek Borzecki <maciek.borzecki@gmail.com>
2011-10-09 13:15:07 +0200

    basic operations (create, gc, add/get config options) work

--- repgit.py
+++ repgit.py
@@ -1,9 +1,172 @@
-import git
+import logging
+import subprocess
+import shlex
+import os
 
-REPO_ROOT = 'repositories/'
-
-def create_repo():
-    """
+class GitException(Exception):
+    """exception wrapper
     """
     pass
+
+def run_cmd(cmd, chdir=None):
+    """
+    run a command and return a tuple retcode, stdout, stderr
+    Arguments:
+    - `cmd`: command string
+    """
+    l = logging.getLogger('git')
+    cmd_l = shlex.split(cmd)
+    l.debug('exec command %s' % (cmd))
+    l.debug('as list %s' % (cmd_l))
+
+    # set directory to current if not defined
+    if not chdir:
+        chdir = os.getcwd()
+    p = subprocess.Popen(cmd_l, cwd=chdir, stdout=subprocess.PIPE, 
+                         stderr=subprocess.PIPE)
+    
+    try:
+        out, err = p.communicate()
+        l.debug('stdout: ' + str(out))
+        l.debug('stderr: ' + str(err))
+    except Exception, e:
+        l.error('failed to run process: %s' % (str(e)))
+        pass
+
+    l.debug('process finished, retcode %d' % (p.returncode))
+    return p.returncode, out, err
+
+def is_git_repo(path):
+    """
+    check if a repository is present under given path
+    Arguments:
+    - `path`:
+    """
+    ret, out, err = run_cmd('git rev-parse --git-dir', path)
+    if ret == 0:
+        return True
+    return False
+
+class Repo(object):
+    """repository wrapper
+    """
+    
+    def __init__(self, path, bare=False):
+        """initialize Repo object, bound to given path
+        NOTE: there are no changes done to repository yet
+        
+        Arguments:
+        - `path`:
+        - `bare`:
+        """
+        l = logging.getLogger('git')
+        l.debug('initializing repository object at %s' % (path))
+        self._path = path
+        self._bare = bare
+        if self._bare:
+            l.debug('repository is bare')
+
+    def bind(self, ):
+        """
+        perform verfication and create repository (if bare)
+        """
+        if self._bare:
+            if os.path.exists(self._path):
+                raise GitException('path %s already present' % (self._path))
+
+            run_cmd('git init --bare %s' % (self._path))
+        else:
+            if not is_git_repo(self._path):
+                raise GitException('%s is not a git repository' % (self._path))
+
+    @classmethod
+    def init_bare(cls, path):
+        """
+        
+        Arguments:
+        - `path`:
+        """
+        
+        r = Repo(path, bare=True)
+        r.bind()
+        return r
+
+    @classmethod
+    def init_existing(cls, path):
+        """
+        
+        Arguments:
+        - `path`:
+        """
+        r = Repo(path)
+        r.bind()
+        return r
+
+    def set_config(self, keys):
+        """set a list of config options
+        
+        Arguments:
+        - `keys`: a map of config options
+        """
+        l = logging.getLogger('git')
+        for (k, v) in keys.iteritems():
+            l.debug('setting %s = %s' % (k, v))
+            run_cmd('git config --local --replace-all %s %s' % (k, v), self._path)
+
+    def get_config(self, keys=None):
+        """get a config option for key
+        
+        Arguments:
+        - `keys`: list of keys to get, empty list or None means all keys
+        """
+        l = logging.getLogger('git')
+        d = {}
+        # get all keys first
+        ret, out, err = run_cmd('git config --local -l', self._path)
+        if ret != 0:
+            raise GitException('failed to obtain configuration')
+        
+        l.debug('wanted keys: %s' % (keys))
+
+        # then filter only ones we are interested in
+        lines = out.strip().split('\n')
+        for l in lines:
+            k, v = l.split('=')
+            if not keys or k in keys:
+                d[k] = v
+
+        return d
+
+    def run_gc(self, ):
+        """run gc on repository
+        """
+        run_cmd('git gc', self._path)
+
+
+def open_repo(path):
+    """create a Repo object bound to given path
+    
+    Arguments:
+    - `path`:
+    """
+    l = logging.getLogger('git')
+    l.info('open a repository at %s' % (path))
+    r = Repo.init_existing(path)
+    return r
+
+def create_bare_repo(path):
+    """create a Repo object for a bare repository. Initialization of a
+    bare repository is performed, hence it should not exist
+    beforehand.
+    
+    Arguments:
+    - `path`:
+    """
+    l = logging.getLogger('git')
+    l.info('create a repository at %s' % (path))
+    r = Repo.init_bare(path)
+    return r
+    
+
+
     
--- repomanager-git.py
+++ repomanager-git.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python
 
+import logging
 import repgit
-import getopt
 import sys
+import os.path
 
+# root directory for all repositories
+REPO_ROOT = 'repositories'
+
+class InvalidParameters(Exception):
+    """
+    """
+    pass
+
+def __extract_repo_name_path(args):
+    """
+    extract repository name and path from args
+    Arguments:
+    - `args`: list of arguments, 0 is always command name
+    """
+    l = logging.getLogger('manager')
+
+    if len(args) < 2:
+        raise InvalidParameters("repository name not provided")
+    name = args[1]
+    if not name.endswith('.git'):
+        l.warning('rename repository from %s to %s' % (name, name + '.git'))
+        name += '.git'
+
+    path = os.path.join(REPO_ROOT, name)
+    return name, path
+    
 def list_repos(args):
     """list repositories
     
     Arguments:
-    - `args`:
+    - `args`: list of arguments, 0 is always command name
     """
+    l = logging.getLogger('manager')
+    l.info('list repositories')
     pass
 
 def create_repo(args):
@@ -18,15 +47,29 @@ def create_repo(args):
     Arguments:
     - `args`:
     """
-    pass
+    l = logging.getLogger('manager')
+    l.info('create repository')
+
+    name, path = __extract_repo_name_path(args)
+
+    repo = repgit.create_bare_repo(path)
+    # fix configuration
+#    repo.set_config()
+
 
 def gc_repo(args):
     """run gc in repository
     
     Arguments:
-    - `args`:
+    - `args`: list of arguments, 0 is always command name
     """
-    pass
+    l = logging.getLogger('manager')
+    l.info('run gc on repository')
+
+    name, path = __extract_repo_name_path(args)
+    
+    repo = repgit.open_repo(path)
+    repo.run_gc()
 
 def show_help(args):
     """show help message
@@ -36,19 +79,65 @@ def show_help(args):
     """
     pass
 
+def set_config(args):
+    """
+    set config option
+    Arguments:
+    - `args`: list of arguments, 0 is always command name
+    """
+    l = logging.getLogger('manager')
+    l.info('set config')
+
+    name, path = __extract_repo_name_path(args)
+    
+    repo = repgit.open_repo(path)
+    # skip args 0 and 1
+    if len(args) < 3:
+        raise Exception('not enough arguments')
+
+    args = args[2:]
+    # the format of key value key value..
+    # even number of arguments is needed
+    if not args or len(args) % 2 != 0:
+        raise Exception('incorrect arguments format')
+    
+    # pack everythong into a dict
+    setc = dict(zip(args[0::2], args[1::2]))
+    
+    repo.set_config(setc)
+
+def get_config(args):
+    """
+    get config option
+    Arguments:
+    - `args`: list of arguments, 0 is always command name
+    """
+    l = logging.getLogger('manager')
+    l.info('get config')
+
+    name, path = __extract_repo_name_path(args)
+    
+    repo = repgit.open_repo(path)
+    conf = repo.get_config()
+    for k, v in conf.iteritems():
+        print '%s -> %s' % (k, v)
+    
 
 def main():
     """
     """
+    logging.basicConfig(level=logging.DEBUG)
     cmnds = { 'help' : show_help,
               'create' : create_repo,
               'list' : list_repos,
-              'gc' : gc_repo
+              'gc' : gc_repo,
+              'setc' : set_config,
+              'getc' : get_config
               }
     args = sys.argv[1:]
 
     if len(args) == 0 or \
-            args[1] not in cmnds:
+            args[0] not in cmnds:
         show_help(args)
         raise SystemExit(1)
     
